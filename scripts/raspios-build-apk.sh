4db40e5e06a955d13df6f643f4543e227870d80b
Stanislav <root-stas@ukr.net>
2026-01-11 17:30:44 +0200

    Fix command syntax for package building

--- scripts/raspios-build-apk.sh
+++ scripts/raspios-build-apk.sh
@@ -111,7 +111,7 @@ CONF
       
       # Build the package with signing
       echo "Building package with abuild..."
-      su builder -c "cd /home/builder/package && abuild -r"
+      su -c "cd /home/builder/package && abuild -r" builder
       
       # The packages should now be signed. Copy them to output
       if [ -d "/home/builder/packages" ]; then
