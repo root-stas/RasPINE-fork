32cf3657e7ac0395220c3ea792df6fce6ea60732
Stanislav <root-stas@ukr.net>
2026-01-11 18:53:20 +0200

    Add ls command to list build directory contents

--- scripts/raspios-build-apk.sh
+++ scripts/raspios-build-apk.sh
@@ -115,8 +115,9 @@ CONF
       # Build the package with signing
       echo "Building package with abuild..."
       echo "root ls"
+      ls /build
       ls /output
-      su -c "cd /home/builder/package && echo "builder ls" && ls /output && abuild -r -C ." builder
+      su -c "cd /home/builder/package && echo 'builder ls' && ls /output && ls /build && abuild -r -C ." builder
       
       # The packages should now be signed. Copy them to output
       if [ -d "/home/builder/packages" ]; then
